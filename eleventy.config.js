8fce8a14b20f60546f60d4c11ea1400352947056
TobiGr <tobigr@users.noreply.github.com>
2025-05-24 20:57:28 +0200

    Add support for static site generator Eleventy (11ty)

---
+++ eleventy.config.js
@@ -0,0 +1,12 @@
+export default function(eleventyConfig) {
+    // Behalte die Originaldateinamen bei und erstelle keine Verzeichnisse für die Seiten
+    eleventyConfig.addGlobalData("permalink", () => {
+        return (data) =>
+            `${data.page.filePathStem}.${data.page.outputFileExtension}`;
+    });
+
+    // Behalte Dateien
+    eleventyConfig.addPassthroughCopy("server.js");
+
+    eleventyConfig.ignores.add("ToDo.md");
+};
